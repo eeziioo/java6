1eca80450e742e5508fab953979ff85c716dd69c
ummaar <155968282+eeziioo@users.noreply.github.com>
2024-01-31 19:44:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let array = ['Ali', 'Ibrohimov', '181 MTT', 'poch', 5]
+
+console.log(array);
+
+array.shift()
+
+array.unshift('Umar')
+
+array[2] = '326 maktab'
+
+array[3] = 'Mars'
+
+array.pop()
+
+array.push(15)
+
+console.log(array);
